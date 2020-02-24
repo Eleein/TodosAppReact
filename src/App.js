c3f314e7061698cbe09b1da198a1db63a0aea717
Dania Duany <deduany@gmail.com>
2020-02-24 19:33:40 +0100

    create todo and todoList

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import styles from "./App.module.scss";
 
 function App() {
+  const [todoName, setTodoName] = useState("");
+  /**
+   * -Create a todo name(just a string )
+   * -Add the todo(object)---props: name, status, created_date, _id to a list of todos onSubmit
+   * -Create a list that maps over the list of todos and displays each one
+   * */
+  function saveTodoName(event) {
+    // const value = event.target.value;
+    const {
+      target: { value: todoName }
+    } = event;
+    setTodoName(todoName);
+  }
+
+  const createTodo = event => {
+    event.preventDefault();
+    const todo = {
+      name: todoName,
+      status: ["pending"],
+      created_date: Date.now(),
+      _id: ""
+    };
+    setTodoName("");
+    setTodos(prevTodos => [todo, ...prevTodos]);
+  };
+
+  const [todos, setTodos] = useState([]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={styles.todoContainer}>
+      <h1>todos</h1>
+      <form className={styles.formToSubmit} onSubmit={createTodo}>
+        <input
+          type="text"
+          placeholder="What needs to be done?"
+          className={styles.todoInput}
+          value={todoName}
+          onChange={saveTodoName}
+        />
+      </form>
+      <ul>
+        {todos.map(todo => (
+          <li>{todo.name}</li>
+        ))}
+      </ul>
     </div>
   );
 }
