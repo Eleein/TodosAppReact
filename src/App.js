b8c168e9397704a95b1af47ccfbbe914ffb85d0e
Dania Duany <deduany@gmail.com>
2020-03-16 18:49:43 +0100

    load todos and delete todos, add styles

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import styles from "./App.module.scss";
 import checkboxIcon from "images/checkbox-icon-png-59.png";
 import uncheckedBox from "images/unchecked-checkbox-icon.png";
+
 function App() {
   const [todoName, setTodoName] = useState("");
   const [todos, setTodos] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:3030/tasks")
+      .then(response => response.json())
+      .then(savedTodos => setTodos(savedTodos))
+      .catch(error => console.log(error));
+  }, []);
+
   /**
    * -Create a todo name(just a string )
    * -Add the todo(object)---props: name, status, created_date, _id to    a list of todos onSubmit
@@ -16,22 +25,37 @@ function App() {
       target: { value: todoName }
     } = event;
 
-      setTodoName(todoName)
-    ;
+    setTodoName(todoName);
   }
   function addTodo(event) {
     event.preventDefault();
-    if(todoName) { const todo = {
-      _id: Math.random().toString(),
-      name: todoName,
-      status: ["pending"],
-      created_date: Date.now()
-    };
+    if (todoName) {
+      const todo = {
+        name: todoName,
+        status: ["pending"],
+        created_date: Date.now()
+      };
 
-    setTodos(prevTodos => {
-      return [todo, ...prevTodos];
-    });
-    setTodoName("")};
+      fetch("http://localhost:3030/tasks", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/JSON"
+        },
+        body: JSON.stringify(todo)
+      })
+        .then(response => {
+          return response.json();
+        })
+        .then(todo => {
+          setTodos(prevTodos => {
+            return [todo, ...prevTodos];
+          });
+          setTodoName("");
+        })
+        .catch(error => {
+          console.log(error);
+        });
+    }
   }
 
   function updateTodos(todoToUpdate) {
@@ -45,7 +69,16 @@ function App() {
   function changeStatusAndUpdateTodos(todoToUpdate) {
     todoToUpdate.status =
       todoToUpdate.status[0] === "pending" ? ["completed"] : ["pending"];
-    updateTodos(todoToUpdate);
+
+    fetch(`http://localhost:3030/tasks/${todoToUpdate._id}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/JSON"
+      },
+      body: JSON.stringify(todoToUpdate)
+    })
+      .then(() => updateTodos(todoToUpdate))
+      .catch(error => console.log(error));
   }
 
   function markAllTodosDone() {
@@ -56,11 +89,23 @@ function App() {
     setTodos(markedTodos);
   }
 
+  function deleteTodo(todoToDelete) {
+   fetch(`http://localhost:3030/tasks/${todoToDelete._id}`, {
+     method: "DELETE",
+     headers: {
+       "Content-Type": "application/JSON"
+     }
+   }).then(() => setTodos(todos.filter( todo => {
+     return todoToDelete._id !== todo._id
+   }))).catch(error => console.log(error))
+  }
+
   return (
     <div className={styles.todoContainer}>
-      <h1>todos</h1>
-      <button onClick={markAllTodosDone}>Mark All Done</button>
+      <h1 className={styles.todoHeader}>todos</h1>
+      
       <form className={styles.formToSubmit} onSubmit={addTodo}>
+        <button onClick={markAllTodosDone}><i className={styles.downArrow}></i></button>
         <input
           type="text"
           placeholder="What needs to be done?"
@@ -71,23 +116,30 @@ function App() {
       </form>
       <ul>
         {todos.map(todo => {
-          const isChecked = todo.status[0] === 'completed';
+          const isChecked = todo.status[0] === "completed";
           return (
-            <li>
+            <li className={styles.todoListItem}>
               <input
+                className={styles.listItemCheckBox}
                 type="checkbox"
                 id={todo._id}
                 onChange={() => {
-
                   changeStatusAndUpdateTodos(todo);
-
                 }}
                 checked={isChecked}
               />
               <label htmlFor={todo._id}>
-                <img src={isChecked ? checkboxIcon : uncheckedBox} alt="checkbox-icon" className={styles.tickBoxImg}/>
+                <img
+                  src={isChecked ? checkboxIcon : uncheckedBox}
+                  alt="checkbox-icon"
+                  className={styles.tickBoxImg}
+                />
               </label>
-              {todo.name}
+              <span className={isChecked ? styles.checkedTodo : ''}>{todo.name}</span>
+              <button type="button" onClick={()=>{
+
+                deleteTodo(todo)
+              }}>X</button>
             </li>
           );
         })}
